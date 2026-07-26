787550914fc7abe305288438683845f0fc8dbcbe
Ambert Jc <jc.ambert@free.fr>
2026-07-26 14:19:35 +0200

    feat: wire doctor command

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import { registerDoctorCommand } from './doctor-command.js';
 import { registerInteractiveStackBrowserCommand } from './interactive-stack-browser-command.js';
 import { createComposeCliProgram } from './program.js';
 import { registerWorkspaceCommands } from './workspace-command.js';
@@ -6,5 +7,6 @@ import { registerWorkspaceCommands } from './workspace-command.js';
 const program = createComposeCliProgram();
 registerInteractiveStackBrowserCommand(program);
 registerWorkspaceCommands(program);
+registerDoctorCommand(program);
 
 await program.parseAsync(process.argv);

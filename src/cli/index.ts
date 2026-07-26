4d14c5a366e1b67542f937982e3606eaa0374624
Ambert Jc <jc.ambert@free.fr>
2026-07-26 13:18:33 +0200

    feat: register workspace commands

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import { registerInteractiveStackBrowserCommand } from './interactive-stack-browser-command.js';
 import { createComposeCliProgram } from './program.js';
+import { registerWorkspaceCommands } from './workspace-command.js';
 
 const program = createComposeCliProgram();
 registerInteractiveStackBrowserCommand(program);
+registerWorkspaceCommands(program);
 
 await program.parseAsync(process.argv);

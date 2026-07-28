3421118284476ad4dc62e3cf849bb09223550440
Ambert Jc <jc.ambert@free.fr>
2026-07-28 11:21:00 +0200

    feat: register compose ui command

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,11 +2,13 @@
 import { registerDoctorCommand } from './doctor-command.js';
 import { registerInteractiveStackBrowserCommand } from './interactive-stack-browser-command.js';
 import { createComposeCliProgram } from './program.js';
+import { registerUiCommand } from './ui-command.js';
 import { registerWorkspaceCommands } from './workspace-command.js';
 
 const program = createComposeCliProgram();
 registerInteractiveStackBrowserCommand(program);
 registerWorkspaceCommands(program);
 registerDoctorCommand(program);
+registerUiCommand(program);
 
 await program.parseAsync(process.argv);

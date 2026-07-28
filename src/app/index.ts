bddc1cd603cd44bfb87ded4f5b85632795ac30d4
Ambert Jc <jc.ambert@free.fr>
2026-07-28 08:42:19 +0200

    refactor: add application service exports

---
+++ src/app/index.ts
@@ -0,0 +1,7 @@
+export * from './compose-command-service.js';
+export * from './compose-file-resolver.js';
+export * from './doctor-service.js';
+export * from './project-service.js';
+export * from './scan-service.js';
+export * from './stack-browser-service.js';
+export * from './workspace-service.js';

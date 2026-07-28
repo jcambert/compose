349d2f236889d676734f3d12d786bb9b4430687b
Ambert Jc <jc.ambert@free.fr>
2026-07-28 16:05:15 +0200

    build: add Vite configuration

---
+++ vite.config.mjs
@@ -0,0 +1,12 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: 'src/ui',
+  plugins: [react()],
+  build: {
+    outDir: '../../dist/ui',
+    emptyOutDir: false,
+    sourcemap: false,
+  },
+});
